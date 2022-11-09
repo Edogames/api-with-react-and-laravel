6f0c55161d012b67f0610930de799d630c7556e3
Edgar <71845364+Edogames@users.noreply.github.com>
2022-11-09 12:20:34 +0400

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,23 +2,23 @@
 
 @section('content')
     @if (count($results) > 0)
-    @for ($i = 0; $i < count($results); $i++)
-        <div class="my-card-body">
-            <h1 class="title">
-                {{$results[$i]->title}}
-            </h1>
-            <p style="color: gray">Description</p>
-            <h3 class="content">
-                {{$results[$i]->content}}
-            </h3>
-            <div class="passed">
-                @if ($results[$i]->passed == 1)
-                    <p style="color: green;">PASSED</p>
-                @else
-                    <p style="color: red;">FAILED</p>
-                @endif
+        @for ($i = 0; $i < count($results); $i++)
+            <div class="my-card-body">
+                <h1 class="title">
+                    {{$results[$i]->title}}
+                </h1>
+                <p style="color: gray">Description</p>
+                <h3 class="content">
+                    {{$results[$i]->content}}
+                </h3>
+                <div class="passed">
+                    @if ($results[$i]->passed == 1)
+                        <p style="color: green;">PASSED</p>
+                    @else
+                        <p style="color: red;">FAILED</p>
+                    @endif
+                </div>
             </div>
-        </div>
         @endfor
     @else
         <h1 style="color: red; text-align: center; margin-top: 40px;">No results yet.</h1>
